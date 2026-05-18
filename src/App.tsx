2a61e7e08ad47ea83dfaeaad0e4f330fd776900e
lakssshhh <laksh69sharma@gmail.com>
2026-05-18 14:04:45 +0530

    added root redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import {Signin} from "./pages/Signin";
-import {Signup} from "./pages/Signup";
-import {Dashboard} from "./pages/dashboard";
-import {Route,Routes, BrowserRouter} from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Dashboard } from "./pages/dashboard";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 
 function App() {
-  return<BrowserRouter>
-  <Routes>
-    <Route path="/signin" element={<Signin/>} />
-    <Route path="/signup" element={<Signup/>} />
-    <Route path="/dashboard" element={<Dashboard/>} />
-  </Routes>
-  </BrowserRouter>
-    
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
