181ae950084d3f7b63f9979a3d2de88fd93e28f6
lakssshhh <laksh69sharma@gmail.com>
2026-05-28 17:09:55 +0530

    dark mode in sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,23 +1,29 @@
 import { SidebarItem } from "./sidebarItem";
 import { Twitter } from "../icons/Twitter";
-import {  YoutubeIcon } from "../icons/YoutubeIcon";
+import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { Logo } from "../icons/Logo";
+
 interface SidebarProps {
     onSelectCategory: (category: string) => void;
 }
 
-export function Sidebar({onSelectCategory}:SidebarProps){
-    return <div className="h-screen bg-white border-slate-200 w-72 fixed left-0 top-0 pl-6">
-        <div className="pt-4 text-2xl font-bold flex items-center">
-            <div className="pr-2 text-blue-800">
-            <Logo/>
+export function Sidebar({onSelectCategory}: SidebarProps) {
+    return (
+        <div className="h-screen bg-white dark:bg-slate-950 border-r border-slate-200 dark:border-slate-800 w-72 fixed left-0 top-0 pl-6 transition-all duration-150">
+            
+            <div className="pt-4 text-2xl font-bold flex items-center dark:text-white">
+                
+            
+                <div className="pr-2 text-blue-800 dark:text-blue-500">
+                    <Logo />
+                </div>
+                Brainly
             </div>
-            Brainly
-        </div>
-        <div className="pt-8 pl-4 ">
-            <SidebarItem onClick={() => onSelectCategory("twitter")} text="Twitter" icon={<Twitter/>}/>
-            <SidebarItem onClick={() => onSelectCategory("youtube")} text="YouTube" icon={<YoutubeIcon/>}/>
 
+            <div className="pt-8 pl-4">
+                <SidebarItem onClick={() => onSelectCategory("twitter")} text="Twitter" icon={<Twitter/>} />
+                <SidebarItem onClick={() => onSelectCategory("youtube")} text="Youtube" icon={<YoutubeIcon/>} />
+            </div>
         </div>
-    </div>
+    )
 }
--- src/components/sidebarItem.tsx
+++ src/components/sidebarItem.tsx
@@ -1,17 +1,21 @@
 import { type ReactElement } from "react";
-export function SidebarItem({text,icon,onClick}:{
-    text:string;
-    icon:ReactElement;
-    onClick:() => void;
-}){
-    return <div className="flex text-gray-700 py-2 items-center cursor-pointer
-    hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-150" onClick={onClick}>
-        <div className="pr-2">
-        {icon}
-        </div>
-        <div className="p-2">
-        {text}
-        </div>
 
-    </div>
+export function SidebarItem({ text, icon, onClick }: {
+    text: string;
+    icon: ReactElement;
+    onClick: () => void;
+}) {
+    return (
+        <div 
+            className="flex text-gray-700 dark:text-gray-300 py-2 items-center cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-800 rounded max-w-48 pl-4 transition-all duration-150" 
+            onClick={onClick}
+        >
+            <div className="pr-2">
+                {icon}
+            </div>
+            <div className="p-2">
+                {text}
+            </div>
+        </div>
+    );
 }
